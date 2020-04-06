857c3ba51945e3ba4449bada5c3041a9bdcf5ed0
Douw Marx <douwmarx@gmail.com>
2020-04-06 19:36:02 +0200

    Initialized most of the scripts an classes required for the diagnostics structure. General layout

---
+++ src/models/lumped_mass_model.py
@@ -0,0 +1,23 @@
+class LMM(object):
+    "CLass for creating lumped mass model that "
+    def __init__(self,model_parameters,fem_surrogate):
+        self.model_parameters = model_parameters
+        self.fem_surrogate = fem_surrogate
+        return
+
+    def solve_de(self):
+        """
+        Solve the differential equation
+        Returns
+        -------
+
+        """
+        return
+
+    def de(self):
+        """
+        Differential equation that describes lumped mass model
+        Returns
+        -------
+
+        """
