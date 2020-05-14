1e888200077e0eacad5bf3144fedcf21636bbc9f
Douw Marx <douwmarx@gmail.com>
2020-05-14 21:42:09 +0200

    Dictionary based optimisation, reformatting of lmm_models.py for dicts

--- models/lumped_mas_model/llm_models.py
+++ models/lumped_mas_model/llm_models.py
@@ -44,7 +44,7 @@ def make_chaari_2006_model():
 
     #  Operating conditions
     ########################################################################################################################
-    Omega_c = 2*np.pi*1285/60#(2*np.pi*8570/60)/(1 + 70/30)#100*2*np.pi*60  # [rad/s]  # Constant angular speed of planet carrier
+    Omega_c = 2 * np.pi * 1285 / 60  # (2*np.pi*8570/60)/(1 + 70/30)#100*2*np.pi*60  # [rad/s]  # Constant angular speed of planet carrier
     T_s = 10  # [N/m]  # Sun torque applied to the sun gear
 
     Opp_atr_ud = {"Omega_c": Omega_c,
@@ -75,6 +75,114 @@ def make_chaari_2006_model():
 
     PG = pglmm.Planetary_Gear(N, M_atr_ud, Geom_atr_ud, k_atr_ud, Opp_atr_ud, solve_atr)
     return PG
+
+
+def make_chaari_2006_model_w_dict():
+    # Chaari
+    # # Number of planet gears
+    # ########################################################################################################################
+    N = 4
+    Z_r = 70
+    Z_s = 30
+    Z_p = 20
+
+    gearbox_layout = {"N": N,
+                      "Z_r:": Z_r,
+                      "Z_s": Z_s,
+                      "Z_p": Z_p}
+
+    #  Mass and inertia
+    ########################################################################################################################
+    m_c = 3.0  # [kg] Carrier mass
+    m_r = 0.588  # [kg] Ring mass
+    m_s = 0.46  # [kg] Sun mass
+    m_p = 0.177  # [kg] Planet 1 mass
+
+    # m1 = 0.177
+    # m_2, m_3, m_4 = m_1, m_1, m_1  # All planet gears have equal mass
+
+    Ir2_c = 1.5  # [kg] Carrier       #These values are I/r^2
+    Ir2_r = 0.759  # [kg] Ring
+    Ir2_s = 0.272  # [kg] Sun
+    Ir2_p = 0.1  # [kg] Planet
+
+    # Ir2_1 = 0.1  # [kg] Planet 1
+    # Ir2_2, Ir2_3, Ir2_4 = Ir2_1, Ir2_1, Ir2_1  # All planet gears have equal I/r^2
+
+    # M_atr_ud = np.array([[m_c, m_r, m_s, m_1, m_2, m_3, m_4],
+    #                     [Ir2_c, Ir2_r, Ir2_s, Ir2_1, Ir2_2, Ir2_3, Ir2_4]])
+
+    m_atr = {"m_c": m_c,
+             "m_r": m_r,
+             "m_s": m_s,
+             "m_p": m_p,
+             "Ir2_c": Ir2_c,
+             "Ir2_r": Ir2_r,
+             "Ir2_s": Ir2_s,
+             "Ir2_p": Ir2_p}
+
+    #  Geometric properties
+    ########################################################################################################################
+    alpha_s = np.deg2rad(21.34)  # Pressure angle at sun gear
+    alpha_r = np.deg2rad(21.34)  # Pressure angle at ring gear
+
+    # Geom_atr_ud = np.array([alpha_s, alpha_r])
+
+    geom_atr = {"alpha_s": alpha_s,
+                "alpha_r": alpha_r}
+
+    # Stiffness properties
+    ########################################################################################################################
+    k_Sp = 2 * 10 ** 8  # [N/m]  # Sun-planet gear mesh stiffness, Peak value?
+    k_rp = 2 * 10 ** 8  # [N/m]  # Ring Planet   gear mesh stiffness
+    k_p = 10 ** 8  # [N/m]  # Bearing stiffness (Planet bearing?)
+    k_ru = 10 ** 9  # [N/m]  # Rotational stiffness preventing ring gear from rotating
+    #    k_atr_ud = np.array([k_Sp, k_rp, k_p, k_ru])
+
+    k_atr = {"k_Sp": k_Sp,
+             "k_rp": k_rp,
+             "k_p": k_p,
+             "k_ru": k_ru}
+
+    #  Operating conditions
+    ########################################################################################################################
+    Omega_c = 2 * np.pi * 1285 / 60  # (2*np.pi*8570/60)/(1 + 70/30)#100*2*np.pi*60  # [rad/s]  # Constant angular speed of planet carrier
+    T_s = 10  # [N/m]  # Sun torque applied to the sun gear
+
+    opp_atr = {"Omega_c": Omega_c,
+               "T_s": T_s,
+               "base_excitation": False}
+
+    #  Solver attributes
+    ######################################################################################################################
+    timerange = np.linspace(0, 0.051, 10000)
+
+    # X0 = np.array([[-1.67427896e-15, 1.81686928e-07, 1.81686884e-08, 3.54914606e-08,
+    #                 -9.08434456e-08, -9.75303772e-09, -3.54914583e-08, -9.08434387e-08,
+    #                 9.88800572e-07, -3.34859651e-15, 3.81542498e-07, -5.11431563e-07]]).T
+    # Xd0 = np.array([[4.10034386e-14, 9.21551453e-09, 9.21668277e-10, 1.80039978e-09,
+    #                  -4.60827729e-09, -4.94760191e-10, -1.80045025e-09, -4.60840343e-09,
+    #                  4.77182740e-08, 8.20079192e-14, 1.93539270e-08, -2.59430020e-08]]).T
+
+    X0 = np.zeros((N * 3 + 9, 1))
+    Xd0 = np.zeros((N * 3 + 9, 1))
+
+    solve_atr = {"solver_alg": "Radau",
+                 "proportional_damping_constant": 0.03,
+                 "time_varying_proportional_damping": False,
+                 "X0": X0,
+                 "Xd0": Xd0,
+                 "time_range": timerange
+                 }
+
+    PG_info = {"gearbox_layout": gearbox_layout,
+               "m_atr": m_atr,
+               "geom_atr": geom_atr,
+               "k_atr": k_atr,
+               "opp_atr": opp_atr,
+               "solve_atr": solve_atr}
+    #    PG = pglmm.Planetary_Gear(N, M_atr_ud, Geom_atr_ud, k_atr_ud, Opp_atr_ud, solve_atr)
+    return PG_info
 
 
 def make_lin_1999_model():
@@ -112,7 +220,6 @@ def make_lin_1999_model():
 
     k_ru = 10 ** 9  # [N/m]  # Rotational stiffness preventing ring gear from rotating
     k_atr_ud = np.array([k_Sp, k_rp, k_p, k_ru])
-
 
     #  Operating conditions
     ########################################################################################################################
@@ -247,7 +354,6 @@ def make_chaari_2006_model_experiment():
     return PG
 
 
-
 def make_bonfiglioli_full_planet():
     # Number of planet gears
     ####################################################################################################################
@@ -255,9 +361,9 @@ def make_bonfiglioli_full_planet():
 
     #  Mass and inertia
     ####################################################################################################################
-    m_c = 1.714 # [kg] Carrier mass
+    m_c = 1.714  # [kg] Carrier mass
     m_r = 1.115  # [kg] Ring mass
-    m_s = 0.242 # [kg] Sun mass
+    m_s = 0.242  # [kg] Sun mass
     m_1 = 0.153  # [kg] Planet 1 mass
 
     Ir2_c = 0.214  # [kg] Carrier       #These values are I/r^2
@@ -287,7 +393,8 @@ def make_bonfiglioli_full_planet():
 
     #  Operating conditions
     ####################################################################################################################
-    Omega_c = (2 * np.pi * 8570 / 60) / (62/13 + 1)  # 100*2*np.pi*60  # [rad/s]  # Constant angular speed of planet carrier 2*np.pi*1285/60#
+    Omega_c = (2 * np.pi * 8570 / 60) / (
+            62 / 13 + 1)  # 100*2*np.pi*60  # [rad/s]  # Constant angular speed of planet carrier 2*np.pi*1285/60#
     T_s = 10  # [N/m]  # Sun torque applied to the sun gear
 
     Opp_atr_ud = {"Omega_c": Omega_c,
@@ -318,6 +425,7 @@ def make_bonfiglioli_full_planet():
 
     PG = pglmm.Planetary_Gear(N, M_atr_ud, Geom_atr_ud, k_atr_ud, Opp_atr_ud, solve_atr)
     return PG
+
 
 def make_chaari_2006_1planet():
     # Number of planet gears
@@ -359,7 +467,7 @@ def make_chaari_2006_1planet():
     #  Operating conditions
     ####################################################################################################################
     Omega_c = (2 * np.pi * 8570 / 60) / (
-                1 + 70 / 30)  # 100*2*np.pi*60  # [rad/s]  # Constant angular speed of planet carrier 2*np.pi*1285/60#
+            1 + 70 / 30)  # 100*2*np.pi*60  # [rad/s]  # Constant angular speed of planet carrier 2*np.pi*1285/60#
     T_s = 10  # [N/m]  # Sun torque applied to the sun gear
 
     Opp_atr_ud = {"Omega_c": Omega_c,
--- src/models/diagnostics.py
+++ src/models/diagnostics.py
@@ -1,12 +1,16 @@
 import numpy as np
 import scipy.optimize as opt
+import collections
+import time
+import matplotlib.pyplot as plt
 
 
 class Inverse_Probelm(object):
     """
     Class for computing the solution to the inverse problem where the optimum crack length is computed for a given measured signal
     """
-    def __init__(self,feature_extraction_object,lmm_object,reconciliation_model_object,dataset_object):
+
+    def __init__(self, feature_extraction_object, lmm_object, reconciliation_model_object, dataset_object):
         """
         Initialize the inverse Problem object
 
@@ -38,3 +42,102 @@ class Inverse_Probelm(object):
         reconciled_features = self.reconciliation_model.get_adjusted_features(measured_features)
 
         return np.linalg.norm(physics_features - reconciled_features)
+
+
+class Diagnostics(object):
+    """
+    Used to find the optimum parameters for a given set of measurements
+    """
+
+    def __init__(self, pg_data, optimize_for):
+        self.data = pg_data  # Depending what should be used in minimizing the cost, this could take a different value
+
+        pdict = {"level1": {"a": 3},
+                 "b": 5,
+                 "c": 2}
+
+        self.optimize_for = optimize_for
+        self.pglmm_info = pdict
+
+        self.opt_for_var = list(self.optimize_for.keys())
+        self.opt_for_bnds = list(self.optimize_for.values())
+
+        return
+
+    def f_min(self, theta):
+
+        # Exchage the values in the dictionary used to initialize the LMM with the values the optimizer wants to use
+        for var_to_opt_for, theta_index in zip(self.opt_for_var, range(len(self.opt_for_var))):
+            edit_dict_var(self.pglmm_info, var_to_opt_for, theta[theta_index])
+
+        f = system(self.pglmm_info)  # Initialize the lumped mass model and get its solution given the model parameters
+
+        return self.cost(f)
+
+    def cost(self, candidate):
+        """
+        The cost function. How is the model punished for not matching the data.
+        Different cost functions can be implemented.
+        Parameters
+        ----------
+        candidate: This is the candidate solution of the model given certain model parameters.
+
+        Returns
+        -------
+
+        """
+        return np.linalg.norm(candidate - self.data)
+
+
+    def do_optimisation(self):
+        sol = opt.differential_evolution(self.f_min,
+                                         self.opt_for_bnds,
+                                         polish=True,
+                                         disp=True)
+        return sol
+
+
+def edit_dict_var(adict, k, v):
+    """
+    Replaces the value of a nested dictionary with a new one.
+    Based on answer by https://stackoverflow.com/users/1481060/sotapme
+    Parameters
+    ----------
+    adict
+    k
+    v
+
+    Returns
+    -------
+
+    """
+    for key in adict.keys():
+        if key == k:
+            adict[key] = v
+        elif type(adict[key]) is dict:
+            edit_dict_var(adict[key], k, v)
+
+
+def system(param_dict):
+    return param_dict["level1"]["a"] * np.linspace(-1, 1, 100) ** 2 + param_dict["b"] * np.linspace(-1, 1, 100) + \
+           param_dict["c"]
+
+
+pdict = {"level1": {"a": 3},
+         "b": 5,
+         "c": 2}
+
+opim_for = {"a": [1, 4],
+            "b": [0, 10]}
+
+measured = np.random.normal(system(pdict), 0.5)
+plt.figure()
+plt.scatter(np.linspace(-1, 1, 100), measured)
+plt.plot(np.linspace(-1, 1, 100), system(pdict))
+
+
+
+diag_obj = Diagnostics(measured, opim_for)
+print(diag_obj.do_optimisation())
+
+plt.plot(np.linspace(-1,1,100), system(diag_obj.pglmm_info))
